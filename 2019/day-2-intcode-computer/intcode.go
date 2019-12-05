813523cd26d26e267b4210764a06a00ff44d0fec
Yuriy Khamzyaev <yuri.rhm@gmail.com>
2019-12-05 21:54:53 +0000

    Implemented IntCode part 1

--- 2019/day-2-intcode-computer/intcode.go
+++ 2019/day-2-intcode-computer/intcode.go
@@ -1,6 +1,69 @@
 package main
 
-// RunIntCode - runs program on IntCode computer
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
+const haltCode = 99
+const addCode = 1
+const multiplyCode = 2
+
+// RunIntCode - runs until halt command and returns final program state
 func RunIntCode(program []int) []int {
-	return []int{30, 10, 20, 0}
+	i := 0
+
+	for {
+		inputCode := program[i]
+
+		if inputCode == haltCode {
+			break
+		}
+
+		outputPosition := program[i + 3]
+		firstOperand := program[program[i + 1]]
+		secondOperand := program[program[i + 2]]
+
+		if inputCode == addCode {
+			program[outputPosition] = firstOperand + secondOperand
+		} else if inputCode == multiplyCode {
+			program[outputPosition] = firstOperand * secondOperand
+		} else {
+			panic(fmt.Sprintf("Error, unhandled opcode: %v", inputCode))
+		}
+
+		i += 4
+	}
+
+	return program
+}
+
+func main() {
+	var program []int
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		line := input.Text()
+		programRaw := strings.Split(line, ",")
+
+		for _, code := range programRaw {
+			number, err := strconv.Atoi(code)
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error converting %s to integer number", line)
+
+				return
+			}
+
+			program = append(program, number)
+		}
+	}
+
+	program[1] = 12
+	program[2] = 2
+
+	fmt.Fprintf(os.Stdout, "Output result of RunIntCode: \n%v\n", RunIntCode(program))
 }
--- 2019/day-2-intcode-computer/intcode_test.go
+++ 2019/day-2-intcode-computer/intcode_test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
-func TestIntCode(t *testing.T) {
-	checkIntCode := func(t *testing.T, program []int, want []int) {
+func TestCalcFuelRequirements(t *testing.T) {
+	checkRequirements := func(t *testing.T, program []int, want []int) {
 		t.Helper()
 
 		got := RunIntCode(program)
@@ -16,9 +16,57 @@ func TestIntCode(t *testing.T) {
 		}
 	}
 
-	t.Run("Should handle adding", func(t *testing.T) {
-		program := []int{1, 10, 20, 0}
+	t.Run("Should support add operation", func(t *testing.T) {
+		program := []int{1, 0, 0, 0, 99}
 
-		checkIntCode(t, program, []int{30, 10, 20, 0})
+		checkRequirements(t, program, []int{2, 0, 0, 0, 99})
+	})
+
+	t.Run("Should support add operation - 2", func(t *testing.T) {
+		program := []int{1, 2, 1, 0, 99}
+
+		checkRequirements(t, program, []int{3, 2, 1, 0, 99})
+	})
+
+	t.Run("Should support add operation - 3", func(t *testing.T) {
+		program := []int{1, 5, 6, 0, 99, 30, 40}
+
+		checkRequirements(t, program, []int{70, 5, 6, 0, 99, 30, 40})
+	})
+
+	t.Run("Should support multiply operation", func(t *testing.T) {
+		program := []int{2, 5, 6, 0, 99, 10, 20}
+
+		checkRequirements(t, program, []int{200, 5, 6, 0, 99, 10, 20})
+	})
+
+	t.Run("Should support multiply operation - 2", func(t *testing.T) {
+		program := []int{2, 5, 6, 0, 99, 30, 40}
+
+		checkRequirements(t, program, []int{1200, 5, 6, 0, 99, 30, 40})
+	})
+
+	t.Run("Should step through program and halt", func(t *testing.T) {
+		program := []int{1, 0, 0, 0, 99}
+
+		checkRequirements(t, program, []int{2, 0, 0, 0, 99})
+	})
+
+	t.Run("Test case 1", func(t *testing.T) {
+		program := []int{2, 3, 0, 3, 99}
+
+		checkRequirements(t, program, []int{2, 3, 0, 6, 99})
+	})
+
+	t.Run("Test case 2", func(t *testing.T) {
+		program := []int{2, 4, 4, 5, 99, 0}
+
+		checkRequirements(t, program, []int{2, 4, 4, 5, 99, 9801})
+	})
+
+	t.Run("Test case 3", func(t *testing.T) {
+		program := []int{1, 1, 1, 4, 99, 5, 6, 0, 99}
+
+		checkRequirements(t, program, []int{30, 1, 1, 4, 2, 5, 6, 0, 99})
 	})
 }
